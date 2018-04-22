d064aae28d5b2f62a5dac29ab9925a18c06fcf92
KylChiang <chiangkaiyun@gmail.com>
2018-04-22 21:24:38 +0800

    Initial Commit

---
+++ CoordinatorTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoordinatorTest
+//
+//  Created by Kyl Chiang on 2018/4/22.
+//  Copyright © 2018年 KaiYun Chiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
